471ded097e15175bb391941bb4e0e8a6149cdf36
rhchaves <rodolfohenriquechaves@gmail.com>
2022-03-18 07:44:48 -0300

    botão teste

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,11 +5,24 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+      <q-btn
+        outline
+        rounded
+        class="float-right"
+        color="primary"
+        label="Botão teste"
+        @click="link"/>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'PageIndex',
+
+  methods: {
+    link() {
+      alert('Clicou aqui');
+    },
+  },
 };
 </script>
